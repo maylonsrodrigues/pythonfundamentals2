36dd8d4fa9f1f5e0d54ce94ffee005866c879ec7
Maylon.rodrigues <maylonsrodrigues@gmail.com>
2020-01-18 16:49:13 -0200

    Re

---
+++ Exercicios/ex_basic.py
@@ -0,0 +1,15 @@
+# #faça um programa que receba 4 notas de alunos 
+# # some e divida por 4
+
+# print(':....programa recebe notas e divide por 4....:')
+
+# n1 = float (input('digite a nota do 1 aluno:_ '))
+# n2 = float (input('digite a nota do 2 aluno:_ '))
+# n3 = float (input('digite a nota do 3 aluno:_ '))
+# n4 = float (input('digite a nota do 4 aluno:_ '))
+# soma = (n1 + n2 + n3 + n4)
+# print (soma, 'é o total das notas...')
+# divi = (soma / 4 )
+# print (divi, 'é o total das notas dividido por 4...')
+# print ('<<<The end>>>')
+
